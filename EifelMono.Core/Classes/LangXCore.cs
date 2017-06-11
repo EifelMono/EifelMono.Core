fd1cf80bb7f9ed1252ec10075c23cc5b609ffa7c
Andreas Klapperich <andreas@klapperich.de>
2017-06-11 22:38:53 +0200

    FIX Lanuage behavior

---
+++ EifelMono.Core/Classes/LangXCore.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EifelMono.Core
+{
+    public class LangXValues
+    {
+        public string ResX { get; protected set; } = "";
+
+        public string FormatText { get; protected set; } = "";
+    }
+}
