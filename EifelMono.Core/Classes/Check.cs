8dd2fcfb957575e1ac50e769f2a8cfba60c25bd0
Andreas Klapperich <andreas@klapperich.de>
2017-06-23 00:28:48 +0200

    Check class added

---
+++ EifelMono.Core/Classes/Check.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace EifelMono.Core.Classes
+{
+    public class Changed<T> where T : IComparable
+    {
+        public Changed()
+        {
+        }
+
+        public Changed(T value)
+        {
+            Value = value;
+
+        }
+
+        T _Value = default(T);
+
+        public T Value
+        {
+            get
+            {
+                return _Value;
+            }
+            set
+            {
+                _Value = value;
+            }
+        };
+
+        public bool IsChanged { get; set; } = false;
+
+
+    }
+}
