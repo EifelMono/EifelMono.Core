1994ee44e24b602c2c887483b18ebbd48b78abf8
Andreas Klapperich <andreas@klapperich.de>
2018-06-27 23:13:20 +0200

    CHG: Refac

--- EifelMono.Core/Extension/JsonExtension.cs
+++ EifelMono.Core/Extension/JsonExtension.cs
@@ -1,10 +1,37 @@
-﻿using System;
+﻿using EifelMono.Core.Log;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace EifelMono.Core.Extension
 {
-    class JsonExtension
+    public static class JsonExtension
     {
+        public static T JsonClone<T>(this T value, T defaultValue = default(T))
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(value));
+            }
+            catch (Exception ex)
+            {
+                ex.LogException();
+                return defaultValue;
+            }
+        }
+
+        public static string ToJsonString(this object value)
+        {
+            try
+            {
+                return JsonConvert.SerializeObject(value, Formatting.Indented);
+            }
+            catch (Exception ex)
+            {
+                ex.LogException();
+                return ex.ToString();
+            }
+        }
     }
 }
